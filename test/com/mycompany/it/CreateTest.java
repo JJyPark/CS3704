a56d9d082a961a4cc829489e28fc273806ae3a17
Silven1117 <Silven1117@hotmail.com>
2017-04-19 16:58:40 -0400

    added 4 system test cases using webdriver/selenium

---
+++ test/com/mycompany/it/CreateTest.java
@@ -0,0 +1,92 @@
+package com.mycompany.it;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class CreateTest {
+
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\Jason\\Documents\\NetBeansProjects\\CS3704\\web\\resources\\geckodriver.exe");
+        driver = new FirefoxDriver();
+        baseUrl = "http://localhost:8080/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+        driver.get(baseUrl + "CS3704/faces/index.xhtml");
+        driver.findElement(By.linkText("Job Applications")).click();
+        driver.findElement(By.id("JobAppListForm:datalist:createButton")).click();
+        driver.findElement(By.id("JobAppCreateForm:time")).clear();
+        driver.findElement(By.id("JobAppCreateForm:time")).sendKeys("930");
+        driver.findElement(By.id("JobAppCreateForm:company")).clear();
+        driver.findElement(By.id("JobAppCreateForm:company")).sendKeys("VT");
+        driver.findElement(By.id("JobAppCreateForm:j_idt65")).click();
+        driver.findElement(By.id("JobAppCreateForm:position")).clear();
+        driver.findElement(By.id("JobAppCreateForm:position")).sendKeys("SDE");
+        driver.findElement(By.id("JobAppCreateForm:location")).clear();
+        driver.findElement(By.id("JobAppCreateForm:location")).sendKeys("Blacksburg");
+        driver.findElement(By.id("JobAppCreateForm:typeOfWork")).clear();
+        driver.findElement(By.id("JobAppCreateForm:typeOfWork")).sendKeys("SDE");
+        driver.findElement(By.id("JobAppCreateForm:j_idt73")).click();
+        assertEquals("930", driver.findElement(By.xpath("//tbody[@id='JobAppListForm:datalist_data']/tr/td[2]")).getText());
+        assertEquals("VT", driver.findElement(By.xpath("//tbody[@id='JobAppListForm:datalist_data']/tr/td[3]")).getText());
+        assertEquals("SDE", driver.findElement(By.xpath("//tbody[@id='JobAppListForm:datalist_data']/tr/td[4]")).getText());
+        assertEquals("Blacksburg", driver.findElement(By.xpath("//tbody[@id='JobAppListForm:datalist_data']/tr/td[5]")).getText());
+        assertEquals("SDE", driver.findElement(By.xpath("//tbody[@id='JobAppListForm:datalist_data']/tr/td[6]")).getText());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
---
+++ test/com/mycompany/it/EditTest.java
@@ -0,0 +1,89 @@
+package com.mycompany.it;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class EditTest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://localhost:8080/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testEdit() throws Exception {
+    driver.get(baseUrl + "/CS3704/faces/index.xhtml");
+    driver.findElement(By.linkText("Job Applications")).click();
+    driver.findElement(By.xpath("//tbody[@id='JobAppListForm:datalist_data']/tr/td[4]")).click();
+    driver.findElement(By.id("JobAppListForm:datalist:editButton")).click();
+    driver.findElement(By.id("JobAppEditForm:time")).clear();
+    driver.findElement(By.id("JobAppEditForm:time")).sendKeys("1030");
+    driver.findElement(By.id("JobAppEditForm:company")).clear();
+    driver.findElement(By.id("JobAppEditForm:company")).sendKeys("VT1");
+    driver.findElement(By.id("JobAppEditForm:position")).clear();
+    driver.findElement(By.id("JobAppEditForm:position")).sendKeys("SDE1");
+    driver.findElement(By.id("JobAppEditForm:location")).clear();
+    driver.findElement(By.id("JobAppEditForm:location")).sendKeys("Blacksburg1");
+    driver.findElement(By.id("JobAppEditForm:typeOfWork")).clear();
+    driver.findElement(By.id("JobAppEditForm:typeOfWork")).sendKeys("SDE1");
+    driver.findElement(By.id("JobAppEditForm:j_idt104")).click();
+    assertEquals("1030", driver.findElement(By.xpath("//tbody[@id='JobAppListForm:datalist_data']/tr/td[2]")).getText());
+    assertEquals("VT1", driver.findElement(By.xpath("//tbody[@id='JobAppListForm:datalist_data']/tr/td[3]")).getText());
+    assertEquals("SDE1", driver.findElement(By.xpath("//tbody[@id='JobAppListForm:datalist_data']/tr/td[4]")).getText());
+    assertEquals("Blacksburg1", driver.findElement(By.xpath("//tbody[@id='JobAppListForm:datalist_data']/tr/td[5]")).getText());
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
